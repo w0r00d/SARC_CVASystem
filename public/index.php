7cf30cfdc7cfe46f2acc5d59ad26ec13f2d4781f
w0r00d <worood.madwar@gmail.com>
2025-05-12 09:09:23 +0300

    modifying index

--- public/index.php
+++ public/index.php
@@ -1,9 +1,6 @@
 <?php
 
-echo 'testing';
 
-exit; 
-/*
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -19,4 +16,3 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
-*/
